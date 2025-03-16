abaced3e1c4c64e23b99492fe7987f3932d78e80
extreme4all <>
2025-03-16 13:48:07 +0100

    Enhance player repository: add methods for creating, updating, and retrieving players

--- components/bot_detector/database/repositories/player.py
+++ components/bot_detector/database/repositories/player.py
@@ -1,4 +1,8 @@
 from bot_detector.database.interfaces import playerInterface
 
 
+# create player
+# update player
+# get player by id
+# get players based on, days since updated, confirmed_Ban, greater than player id with a limit
 class playerRepo(playerInterface): ...
