7b67303c14e45d8110fac9c91574db828eab486f
extreme4all <>
2025-04-17 22:24:12 +0200

    cleanup

--- components/bot_detector/kafka/__init__.py
+++ components/bot_detector/kafka/__init__.py
@@ -1,11 +1,5 @@
 from pydantic_settings import BaseSettings
 
-from .consumer import KafkaConsumer
-from .producer import KafkaProducer
-
 
 class Settings(BaseSettings):
     KAFKA_BOOTSTRAP_SERVERS: str
-
-
-__all__ = ["KafkaConsumer", "KafkaProducer", "Settings"]
