8613c9f71b76b69dc3940d11805a1a4d1493bd60
extreme4all <>
2025-05-29 16:58:51 +0200

    rate-limit-check

--- components/bot_detector/runemetrics_api/exceptions.py
+++ components/bot_detector/runemetrics_api/exceptions.py
@@ -4,3 +4,9 @@ class UnexpectedRedirection(Exception):
 
 class Undefined(Exception):
     pass
+
+
+class RateLimitExceeded(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
