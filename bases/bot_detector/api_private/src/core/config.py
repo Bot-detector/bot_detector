dfc6582b03dcd41be5817e2a124a68992ebda2f7
RusticPotato <battaglmb@gmail.com>
2025-06-07 18:20:58 -0400

    kafka setting is not being used anywhere?, removing

--- bases/bot_detector/api_private/src/core/config.py
+++ bases/bot_detector/api_private/src/core/config.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     ENV: str
     DATABASE_URL: str
-    KAFKA_HOST: str
     POOL_RECYCLE: int = 25
     POOL_TIMEOUT: int = 25
+
 
 settings = Settings()
