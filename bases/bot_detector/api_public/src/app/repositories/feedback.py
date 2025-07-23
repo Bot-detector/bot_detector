df8c99f6681bb1db061f87cd29511dae64769de7
extreme4all <>
2025-07-24 00:19:17 +0200

    modified:   bases/bot_detector/api_public/src/app/repositories/feedback.py

--- bases/bot_detector/api_public/src/app/repositories/feedback.py
+++ bases/bot_detector/api_public/src/app/repositories/feedback.py
@@ -41,7 +41,7 @@ class Feedback:
 
         async with self.session:
             result: AsyncResult = await self.session.execute(sql_select)
-            result = result.first()
+            result = result.mappings().first()
 
             # check if voter exists
             if not result:
@@ -49,7 +49,6 @@ class Feedback:
                 await self.session.rollback()
                 return False, "voter_does_not_exist"
 
-            print(result)
             voter_id = result["id"]
             sql_dupe_check = sql_dupe_check.where(dbFeedback.voter_id == voter_id)
 
