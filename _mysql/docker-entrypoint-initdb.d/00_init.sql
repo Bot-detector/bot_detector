9e2172fc4fa8811bb7e8fe835af4ecdb342f5f6a
extreme4all <>
2025-08-12 23:16:48 +0200

    fix user permissions

--- _mysql/docker-entrypoint-initdb.d/00_init.sql
+++ _mysql/docker-entrypoint-initdb.d/00_init.sql
@@ -2,3 +2,4 @@ CREATE DATABASE playerdata;
 
 CREATE USER `report-worker`@`%` IDENTIFIED BY 'report_worker_pw';
 CREATE USER `hiscore-worker`@`%` IDENTIFIED BY 'hiscore_worker_pw';
+CREATE USER `job-prune-hs`@`%` IDENTIFIED BY 'job_prune_hs_pw';
