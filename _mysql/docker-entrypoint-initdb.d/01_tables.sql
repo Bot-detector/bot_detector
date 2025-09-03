d85bf1ff9941fd57d2a9e02bd6a5d3a1b4d3961a
extreme4all <>
2025-09-03 23:01:11 +0200

    bf

--- _mysql/docker-entrypoint-initdb.d/01_tables.sql
+++ _mysql/docker-entrypoint-initdb.d/01_tables.sql
@@ -270,4 +270,48 @@ CREATE TABLE `scraper_data_v3` (
   PRIMARY KEY (`scrape_id`),
   UNIQUE KEY `unique_player_scrape` (`player_id`,`scrape_date`),
   KEY `idx_scrape_ts` (`scrape_ts`)
-)
+);
+
+CREATE TABLE `scraper_player_skill` (
+  `scrape_id` bigint unsigned NOT NULL,
+  `player_skill_id` bigint unsigned NOT NULL,
+  PRIMARY KEY (`scrape_id`,`player_skill_id`),
+  KEY `idx_player_skill_id` (`player_skill_id`)
+);
+
+CREATE TABLE `scraper_player_activity` (
+  `scrape_id` bigint unsigned NOT NULL,
+  `player_activity_id` bigint unsigned NOT NULL,
+  PRIMARY KEY (`scrape_id`,`player_activity_id`),
+  KEY `idx_player_activity_id` (`player_activity_id`)
+);
+
+CREATE TABLE `player_skill` (
+  `player_skill_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `skill_id` tinyint unsigned NOT NULL,
+  `skill_value` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`player_skill_id`),
+  UNIQUE KEY `unique_skill_value` (`skill_id`,`skill_value`)
+);
+
+CREATE TABLE `player_activity` (
+  `player_activity_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `activity_id` tinyint unsigned NOT NULL,
+  `activity_value` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`player_activity_id`),
+  UNIQUE KEY `unique_activity_value` (`activity_id`,`activity_value`)
+);
+
+CREATE TABLE `skill` (
+  `skill_id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `skill_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`skill_id`),
+  UNIQUE KEY `unique_skill_name` (`skill_name`)
+);
+
+CREATE TABLE `activity` (
+  `activity_id` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `activity_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`activity_id`),
+  UNIQUE KEY `unique_activity_name` (`activity_name`)
+);
