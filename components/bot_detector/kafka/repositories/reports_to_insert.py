0319307521c15a3565afa8503b731b1c480da89e
extreme4all <40169115+extreme4all@users.noreply.github.com>
2025-12-20 22:45:27 +0100

    bf/handle-kafka-timeout-error

--- components/bot_detector/kafka/repositories/reports_to_insert.py
+++ components/bot_detector/kafka/repositories/reports_to_insert.py
@@ -1,8 +1,10 @@
+import asyncio
 import logging
 import time
 
 import orjson
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer, TopicPartition
+from aiokafka.errors import KafkaTimeoutError
 from bot_detector.kafka.interface import (
     ConsumerInterface,
     ProducerInterface,
@@ -165,6 +167,7 @@ class RepoReportsToInsertProducer(ProducerInterface):
             acks="all",
         )
         self.topic = "reports.to_insert"
+        self.semaphore = asyncio.Semaphore(value=10)
 
     async def start(self):
         await self.producer.start()
@@ -180,7 +183,16 @@ class RepoReportsToInsertProducer(ProducerInterface):
         if not isinstance(report, ReportsToInsertStruct):
             raise Exception()
 
-        await self.producer.send(
-            topic=self.topic,
-            value=report.model_dump(),
-        )
+        retries = 0
+        MAX_BACKOFF = 60
+        while True:
+            async with self.semaphore:
+                try:
+                    await self.producer.send(
+                        topic=self.topic,
+                        value=report.model_dump(),
+                    )
+                except KafkaTimeoutError:
+                    retries += 1
+                    logger.warning(f"KafkaTimeoutError - {retries=} ")
+                    await asyncio.sleep(min(2**retries, MAX_BACKOFF))
