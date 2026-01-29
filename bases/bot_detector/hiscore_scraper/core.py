ea8838506e9b77bc908ba66778331b8f2b88e392
extreme4all <>
2026-01-30 00:58:22 +0100

    bf/cleaner-working

--- bases/bot_detector/hiscore_scraper/core.py
+++ bases/bot_detector/hiscore_scraper/core.py
@@ -1,8 +1,6 @@
 import asyncio
 import logging
-import time
 from datetime import date, datetime, timedelta
-from typing import Any
 
 import aiohttp
 from aiohttp import ClientSession
@@ -44,28 +42,20 @@ from .retry_tracker import RetryTracker
 logger = logging.getLogger(__name__)
 
 
-async def get_proxy(
-    proxy_manager: ProxyManager,
-    worker_id: int,
-) -> str | None:
-    proxy, error = await proxy_manager.get_proxy(worker_id)
-
-    if error:
-        logger.error(f"Worker {worker_id}: {error}")
-        return None
-
-    if isinstance(proxy, list):
-        logger.error(f"Worker {worker_id}: Proxy is a list, expected a string.")
-        return None
-    return proxy
-
-
 async def scrape_player(
+    worker_id: int,
     player: PlayerStruct,
     session: ClientSession,
     hiscore_instance: Hiscore,
-) -> tuple[PlayerStats | None, float | None, str | None]:
-    player_stats, error, latency = None, None, None
+    proxy: str,
+    player_nf_producer: PlayersNotFoundProducer,
+    player_ts_producer: PlayersToScrapeProducer,
+) -> tuple[PlayerStats | None, bool]:
+    """
+    Scrape player stats from hiscores.
+    Returns a tuple of (PlayerStats | None, bool) where the bool indicates
+    whether to retry scraping the player.
+    """
     try:
         hiscore_data = await hiscore_instance.get(
             mode=HSMode.OLDSCHOOL,
@@ -75,31 +65,54 @@ async def scrape_player(
         )
         if isinstance(hiscore_data, tuple):
             player_stats, latency = hiscore_data
+            latency_histogram.labels(proxy=proxy).observe(latency)
         else:
             player_stats = hiscore_data
-        return player_stats, latency, error
+        return player_stats, False
     except PlayerDoesNotExist:
-        logger.debug(f"{player.name=} does not exist.")
-        return None, None, None
+        not_found_counter.labels(proxy=proxy).inc()
+        logger.debug(f"[{worker_id}][{player.name}]: not found.")
+        player.possible_ban = True
+        await player_nf_producer.produce_one(
+            NotFoundStruct(
+                metadata=MetaData(version=1, source="hiscore_scraper"),
+                player_data=player,
+            )
+        )
+        return None, False
     except UnexpectedRedirection as e:
-        error = f"Unexpected redirection: {e}"
-        # logger.error(error)
-        return None, None, error
+        error_counter.labels(proxy=proxy).inc()
+        logger.warning(f"[{worker_id}][{player.name}]: {e=}")
+        await player_ts_producer.produce_one(
+            ToScrapeStruct(
+                metadata=MetaData(version=1, source="hiscore_scraper"),
+                player_data=player,
+            )
+        )
+        return None, True
     except (
         aiohttp.ClientResponseError,
         aiohttp.ConnectionTimeoutError,
         aiohttp.ClientConnectorError,
         aiohttp.ServerDisconnectedError,
+        asyncio.TimeoutError,
     ) as e:
-        error = f"Client response error: {e}"
-        # logger.error(error)
-        return None, None, error
+        error_counter.labels(proxy=proxy).inc()
+        logger.warning(f"[{worker_id}][{player.name}]: {e=}")
+        await player_ts_producer.produce_one(
+            ToScrapeStruct(
+                metadata=MetaData(version=1, source="hiscore_scraper"),
+                player_data=player,
+            )
+        )
+        return None, True
 
 
 async def transform_player_stats(
     player_stats: PlayerStats,
     player: PlayerStruct,
-) -> tuple[ScrapedStruct | None, Any | None]:
+    player_ts_producer: PlayersToScrapeProducer,
+) -> ScrapedStruct | None:
     player.updated_at = datetime.now()
     player.possible_ban = False
     player.confirmed_ban = False
@@ -120,11 +133,17 @@ async def transform_player_stats(
                 time_to_live=date.today() + timedelta(days=30),
             ),
         )
+        return hiscore_data
     except ValidationError as e:
         error = e.json()
         logger.error(error)
-        return None, error
-    return hiscore_data, error
+        await player_ts_producer.produce_one(
+            ToScrapeStruct(
+                metadata=MetaData(version=1, source="hiscore_scraper"),
+                player_data=player,
+            )
+        )
+        return None
 
 
 async def handle_retry(
@@ -151,48 +170,67 @@ async def handle_retry(
     await asyncio.sleep(delay)
 
 
+async def get_proxy(
+    worker_id: int,
+    proxy_manager: ProxyManager,
+) -> str | None:
+    proxy, error = await proxy_manager.get_proxy(worker_id)
+    if isinstance(proxy, list):
+        logger.error(f"Worker {worker_id}: Proxy is a list: {proxy}")
+        return None
+    elif proxy is None:
+        logger.error(f"Worker {worker_id}: No proxy available.")
+        return None
+    elif error:
+        logger.error(f"Worker {worker_id}: Error getting proxy: {error}")
+        _error = f"Worker {worker_id}: {error}"
+        logger.error(_error)
+        return None
+    return proxy
+
+
+async def get_player_to_scrape(
+    worker_id: int,
+    player_ts_consumer: PlayersToScrapeConsumer,
+) -> ToScrapeStruct | None:
+    player_to_scrape, error = await player_ts_consumer.consume_one()
+    if error:
+        logger.error(f"[{worker_id}]: Error consuming player to scrape: {error}")
+        return None
+    if player_to_scrape is None:
+        logger.warning(f"[{worker_id}]: No player available.")
+        return None
+    return player_to_scrape
+
+
 async def work(
     worker_id: int,
     proxy_manager: ProxyManager,
-    rate_limiter: RateLimiter,
     player_ts_consumer: PlayersToScrapeConsumer,
     player_ts_producer: PlayersToScrapeProducer,
     player_nf_producer: PlayersNotFoundProducer,
     player_sc_producer: PlayersScrapedProducer,
 ):
-    retry_tracker = RetryTracker(base_delay=10.0, max_delay=300.0, decay_window=300.0)
+    retry_tracker = RetryTracker(base_delay=1.0, max_delay=300.0, decay_window=300.0)
+    rate_limiter = RateLimiter(
+        calls_per_interval=ProxySettings().MAX_CALLS,  # type: ignore
+        interval=ProxySettings().INTERVAL,  # type: ignore
+    )
 
-    async with ClientSession() as session:
+    async with ClientSession(
+        headers={"User-Agent": "http://osrsbotdetector.com"}
+    ) as session:
         while True:
-            # get proxy
-            proxy = await get_proxy(proxy_manager, worker_id)
-
-            # handle exceptions
+            proxy = await get_proxy(worker_id, proxy_manager)
             if proxy is None:
-                logger.error(f"[{worker_id}]: No proxy available.")
                 await asyncio.sleep(10)
                 continue
 
             _proxy = proxy.split("@")[1]
 
             # get player from kafka
-            try:
-                start_time = time.perf_counter()
-                player_to_scrape, error = await player_ts_consumer.consume_one()
-                total_time = time.perf_counter() - start_time
-                logger.debug(f"[{worker_id}]: consume one {total_time:.4f}")
-            except ValidationError as e:
-                logger.error(e.json())
-                continue
-
-            # handle exceptions
-            if error:
-                logger.error(f"[{worker_id}]: {error}")
-                await asyncio.sleep(10)
-                continue
-
+            player_to_scrape = await get_player_to_scrape(worker_id, player_ts_consumer)
             if player_to_scrape is None:
-                logger.warning(f"[{worker_id}]: No player available.")
                 await asyncio.sleep(10)
                 continue
 
@@ -206,63 +244,30 @@ async def work(
             # metric: every time we scrape a player, we increment the counter
             total_counter.labels(proxy=_proxy).inc()
 
-            player_stats, latency, error = await scrape_player(
+            player_stats, retry = await scrape_player(
                 player=player_data,
                 session=session,
                 hiscore_instance=hiscore_instance,
+                proxy=_proxy,
+                worker_id=worker_id,
+                player_nf_producer=player_nf_producer,
+                player_ts_producer=player_ts_producer,
             )
-
-            if latency:
-                latency_histogram.labels(proxy=_proxy).observe(latency)
-                logger.debug(f"[{worker_id}]: scrape one {latency:.4f}")
-
-            # handle exceptions
-            if error:
-                error_counter.labels(proxy=_proxy).inc()
-                retry_count = retry_tracker.get_retry_count(worker_id)
-                logger.warning(
-                    f"[{worker_id}][{player_data.name}]: {error=}, "
-                    f"retry_count={retry_count}"
-                )
-                await player_ts_producer.produce_one(
-                    ToScrapeStruct(
-                        metadata=MetaData(version=1, source="hiscore_scraper"),
-                        player_data=player_to_scrape.player_data,
-                    )
-                )
-                await handle_retry(retry_tracker, worker_id, proxy)
-                continue
-
-            # if player not found, than send to not found topic
             if player_stats is None:
-                not_found_counter.labels(proxy=_proxy).inc()
-                logger.debug(f"[{worker_id}][{player_data.name}]: not found.")
-                player_data.possible_ban = True
-                await player_nf_producer.produce_one(
-                    NotFoundStruct(
-                        metadata=MetaData(version=1, source="hiscore_scraper"),
-                        player_data=player_data,
-                    )
-                )
-                await handle_retry(retry_tracker, worker_id, proxy)
+                if retry:
+                    await handle_retry(retry_tracker, worker_id, proxy)
                 continue
 
             success_counter.labels(proxy=_proxy).inc()
 
             # transform player stats to hiscore data
-            scraped_data, error = await transform_player_stats(
-                player_stats=player_stats, player=player_data
+            scraped_data = await transform_player_stats(
+                player_stats=player_stats,
+                player=player_data,
+                player_ts_producer=player_ts_producer,
             )
 
-            if error:
-                logger.error(f"[{worker_id}][{player_data.name}]: Error transforming.")
-                await player_ts_producer.produce_one(player_to_scrape)
-                await handle_retry(retry_tracker, worker_id, proxy)
-                continue
-
             if scraped_data is None:
-                logger.error(f"[{worker_id}][{player_data.name}]: No scraped data.")
-                await player_ts_producer.produce_one(player_to_scrape)
                 await handle_retry(retry_tracker, worker_id, proxy)
                 continue
 
@@ -302,10 +307,6 @@ async def main():
         work(
             worker_id=worker_id,
             proxy_manager=proxy_manager,
-            rate_limiter=RateLimiter(
-                calls_per_interval=ProxySettings().MAX_CALLS,  # type: ignore
-                interval=ProxySettings().INTERVAL,  # type: ignore
-            ),
             player_ts_consumer=player_ts_consumer,
             player_ts_producer=player_ts_producer,
             player_nf_producer=player_nf_producer,
