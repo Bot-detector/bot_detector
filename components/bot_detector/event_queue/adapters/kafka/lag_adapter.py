d6bf6f72a0aa394762095628f2df6b0fecac7402
extreme4all <>
2026-02-22 14:25:48 +0100

    bf/consumer-not-subscribe

--- components/bot_detector/event_queue/adapters/kafka/lag_adapter.py
+++ components/bot_detector/event_queue/adapters/kafka/lag_adapter.py
@@ -1,5 +1,4 @@
 from aiokafka import AIOKafkaConsumer, TopicPartition
-
 from bot_detector.event_queue.lag_probe.interface import LagProbeProtocol
 
 
@@ -8,16 +7,13 @@ class KafkaLagProbe(LagProbeProtocol):
         self.bootstrap_servers = bootstrap_servers
         self._consumers: dict[str, AIOKafkaConsumer] = {}
 
-    async def start(self) -> None:
-        return None
-
     async def stop(self) -> None:
         for consumer in self._consumers.values():
             await consumer.stop()
         self._consumers.clear()
 
     async def lag(self, topic: str, group_id: str) -> int:
-        consumer = await self._get_or_create_consumer(topic=topic, group_id=group_id)
+        consumer = await self._get_or_create_consumer(group_id=group_id)
 
         partitions = consumer.partitions_for_topic(topic)
         if partitions is None:
@@ -33,14 +29,12 @@ class KafkaLagProbe(LagProbeProtocol):
 
     async def _get_or_create_consumer(
         self,
-        topic: str,
         group_id: str,
     ) -> AIOKafkaConsumer:
         if group_id in self._consumers:
             return self._consumers[group_id]
 
         consumer = AIOKafkaConsumer(
-            topic,
             bootstrap_servers=self.bootstrap_servers,
             group_id=group_id,
             enable_auto_commit=False,
