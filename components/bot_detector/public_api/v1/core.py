8d9ccb25f72b96c9fa191a9ef304af63e19d358e
extreme4all <>
2026-05-01 16:55:43 +0200

    fix url's

--- components/bot_detector/public_api/v1/core.py
+++ components/bot_detector/public_api/v1/core.py
@@ -15,7 +15,7 @@ from bot_detector.rate_limiter import RateLimiter
 
 
 class LegacyApiClient:
-    DEFAULT_BASE_URL = "https://api-v1.prd.osrsbotdetector.com/"
+    DEFAULT_BASE_URL = "https://api-v1.prd.osrsbotdetector.com"
 
     def __init__(
         self,
@@ -33,7 +33,7 @@ class LegacyApiClient:
     async def get_project_stats(self) -> Any:
         await self.limiter.check()
         async with self.session.get(
-            self.base_url + "site/dashboard/projectstats",
+            self.base_url + "/site/dashboard/projectstats",
         ) as res:
             res.raise_for_status()
             return orjson.loads(await res.read())
@@ -42,7 +42,7 @@ class LegacyApiClient:
     async def verify_bot(self, bots: Bots) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + f"site/verify/{self.token}",
+            self.base_url + f"/site/verify/{self.token}",
             data=orjson.dumps(bots.model_dump()),
             headers={"Content-Type": "application/json"},
         ) as res:
@@ -53,7 +53,7 @@ class LegacyApiClient:
     async def get_player(self, player_name: str) -> dict[str, Any] | None:
         await self.limiter.check()
         async with self.session.get(
-            self.base_url + "v1/player",
+            self.base_url + "/v1/player",
             params={"token": self.token, "player_name": player_name},
         ) as res:
             res.raise_for_status()
@@ -66,7 +66,7 @@ class LegacyApiClient:
     async def create_player(self, name: str) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + "v1/player",
+            self.base_url + "/v1/player",
             params={"player_name": name, "token": self.token},
         ) as res:
             res.raise_for_status()
@@ -77,7 +77,7 @@ class LegacyApiClient:
         await self.limiter.check()
         async with self.session.get(
             self.base_url
-            + f"discord/verify/player_rsn_discord_account_status/{self.token}/{player_name}",
+            + f"/discord/verify/player_rsn_discord_account_status/{self.token}/{player_name}",
         ) as res:
             res.raise_for_status()
             return orjson.loads(await res.read())
@@ -90,7 +90,7 @@ class LegacyApiClient:
         await self.limiter.check()
         async with self.session.get(
             self.base_url
-            + f"discord/verify/get_verification_attempts/{self.token}/{player_name}",
+            + f"/discord/verify/get_verification_attempts/{self.token}/{player_name}",
         ) as res:
             res.raise_for_status()
             return orjson.loads(await res.read())
@@ -99,7 +99,7 @@ class LegacyApiClient:
     async def post_verification_request(self, info: DiscordVerifyInfo) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + f"discord/verify/insert_player_dpc/{self.token}",
+            self.base_url + f"/discord/verify/insert_player_dpc/{self.token}",
             data=orjson.dumps(info.model_dump()),
             headers={"Content-Type": "application/json"},
         ) as res:
@@ -118,7 +118,7 @@ class LegacyApiClient:
     async def get_linked_accounts(self, discord_id: int) -> Any:
         await self.limiter.check()
         async with self.session.get(
-            self.base_url + f"discord/get_linked_accounts/{self.token}/{discord_id}",
+            self.base_url + f"/discord/get_linked_accounts/{self.token}/{discord_id}",
         ) as res:
             res.raise_for_status()
             return orjson.loads(await res.read())
@@ -130,7 +130,7 @@ class LegacyApiClient:
     async def get_xp_gains(self, player_name: str) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + f"discord/get_xp_gains/{self.token}",
+            self.base_url + f"/discord/get_xp_gains/{self.token}",
             data=orjson.dumps(PlayerName(player_name=player_name).model_dump()),
             headers={"Content-Type": "application/json"},
         ) as res:
@@ -141,7 +141,7 @@ class LegacyApiClient:
     async def get_latest_sighting(self, player_name: str) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + f"discord/get_latest_sighting/{self.token}",
+            self.base_url + f"/discord/get_latest_sighting/{self.token}",
             data=orjson.dumps(PlayerName(player_name=player_name).model_dump()),
             headers={"Content-Type": "application/json"},
         ) as res:
@@ -152,7 +152,7 @@ class LegacyApiClient:
     async def get_region(self, region_name: str) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + f"discord/region/{self.token}",
+            self.base_url + f"/discord/region/{self.token}",
             data=orjson.dumps(RegionName(region_name=region_name).model_dump()),
             headers={"Content-Type": "application/json"},
         ) as res:
@@ -166,7 +166,7 @@ class LegacyApiClient:
     async def get_heatmap_data(self, region_id: int) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + f"discord/heatmap/{self.token}",
+            self.base_url + f"/discord/heatmap/{self.token}",
             data=orjson.dumps(RegionID(region_id=region_id).model_dump()),
             headers={"Content-Type": "application/json"},
         ) as res:
@@ -177,7 +177,7 @@ class LegacyApiClient:
     async def get_hiscore_latest(self, player_id: int) -> list[dict[str, Any]] | None:
         await self.limiter.check()
         async with self.session.get(
-            self.base_url + "v1/hiscore/Latest",
+            self.base_url + "/v1/hiscore/Latest",
             params={"token": self.token, "player_id": player_id},
         ) as res:
             res.raise_for_status()
@@ -187,7 +187,7 @@ class LegacyApiClient:
     async def generate_player_bans_export(self, export: ExportInfo) -> Any:
         await self.limiter.check()
         async with self.session.post(
-            self.base_url + f"discord/player_bans/{self.token}",
+            self.base_url + f"/discord/player_bans/{self.token}",
             data=orjson.dumps(export.model_dump()),
             headers={"Content-Type": "application/json"},
         ) as res:
@@ -198,7 +198,7 @@ class LegacyApiClient:
     async def download_export(self, export_id: str) -> Any:
         await self.limiter.check()
         async with self.session.get(
-            self.base_url + f"discord/download_export/{export_id}",
+            self.base_url + f"/discord/download_export/{export_id}",
         ) as res:
             res.raise_for_status()
             return orjson.loads(await res.read())
