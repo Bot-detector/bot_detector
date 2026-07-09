d5bb7dfb080d66e2cc3bb1bd87a2bf3c80f22e9e
extreme4all <>
2026-07-09 22:41:03 +0200

    style: format report __all__ multi-line

--- components/bot_detector/database/report/__init__.py
+++ components/bot_detector/database/report/__init__.py
@@ -3,4 +3,9 @@ from .migration import migrate_banned_player_reports
 from .repository import ReportRepo
 from .retention import prune_reports
 
-__all__ = ["ReportInterface", "ReportRepo", "migrate_banned_player_reports", "prune_reports"]
+__all__ = [
+    "ReportInterface",
+    "ReportRepo",
+    "migrate_banned_player_reports",
+    "prune_reports",
+]
