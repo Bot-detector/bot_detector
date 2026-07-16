fec65a0bae15107c38fc508ce88d5b0dfa7fcc8d
extreme4all <>
2026-07-16 23:44:30 +0200

    discord.http logging

--- bases/bot_detector/discord_bot/__init__.py
+++ bases/bot_detector/discord_bot/__init__.py
@@ -1,4 +1,12 @@
+import logging
+
 from bot_detector import logfmt
 from bot_detector.discord_bot.core import run
 
 __all__ = ["logfmt", "run"]
+
+# discord.py loggers
+logging.getLogger("discord.http").setLevel(logging.DEBUG)
+logging.getLogger("discord").setLevel(logging.INFO)
+logging.getLogger("discord.gateway").setLevel(logging.INFO)
+logging.getLogger("discord.client").setLevel(logging.INFO)
